b62f62920e24c9e1736ff7852d5b47ae44c16865
Gulam Kibria Chowdhury <43819400+GK-CPP@users.noreply.github.com>
2023-09-20 17:22:12 +0600

    get data form and call store fun

---
+++ products/create.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once '../utils.php';
+require_once('../src/models/Product.php');
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    if (isset($_POST['submit'])) {
+        $name = $_POST["name"];
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+        $role = $_POST["role"];
+        $description = $_POST["description"];
+
+        // dd($name);
+        $product = new Product($name, $email, $password, $role, $description);
+        $product->store();
+    }
+}
