a4c4056bcba95e54f15c6b8b514f617cfdd47813
Gulam Kibria Chowdhury <43819400+GK-CPP@users.noreply.github.com>
2023-09-20 19:28:30 +0600

    header (not ready)

--- layouts/header.php
+++ layouts/header.php
@@ -3,3 +3,32 @@
     <h1 class="font-bold text-2xl italic text-center text-white">CRUD Operation In PHP Using OOPs</h1>
 
 </header>
+
+<nav class="bg-gray-800 text-white p-4">
+    <ul class="flex justify-center space-x-4">
+        <li>
+            <a href="./show.php" class="flex flex-col items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+                </svg>
+                Home
+            </a>
+        </li>
+        <li>
+            <a href="../index.php" class="flex flex-col items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
+                </svg>
+                Add
+            </a>
+        </li>
+        <li>
+            <a href="../login.php" class="flex flex-col items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
+                </svg>
+                Login
+            </a>
+        </li>
+    </ul>
+</nav>
