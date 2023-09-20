22d60ea7d77e92584dc0616c1c47bee4c97ebb53
Gulam Kibria Chowdhury <43819400+GK-CPP@users.noreply.github.com>
2023-09-20 19:27:01 +0600

    signup

---
+++ signUp.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign Up with Google</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.15/dist/tailwind.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <!--include header   -->
+    <?php include("./layouts/header.php"); ?>
+
+    <div class=" bg-gray-200 min-h-full mx-auto w-full flex justify-center">
+        <div class="bg-white shadow-md rounded-lg p-6 w-96 items-center justify-center">
+            <h1 class="text-2xl font-semibold mb-4">Sign Up</h1>
+            <!-- Sign Up Form -->
+            <form action="#" method="post">
+                <div class="mb-4">
+                    <label for="name" class="block text-gray-700 text-sm font-bold mb-2">Name</label>
+                    <input type="text" name="name" id="name" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Enter Name">
+                </div>
+                <div class="mb-4">
+                    <label for="email" class="block text-gray-700 text-sm font-bold mb-2">Email</label>
+                    <input type="text" name="email" id="email" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Enter Email">
+                </div>
+                <div class="mb-4">
+                    <label for="password" class="block text-gray-700 text-sm font-bold mb-2">Password</label>
+                    <input type="password" name="password" id="password" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Enter Password">
+                </div>
+                <div class="flex justify-between items-center">
+                    <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Sign Up</button>
+                    <a href="https://accounts.google.com/o/oauth2/auth?client_id=YOUR_CLIENT_ID&redirect_uri=YOUR_REDIRECT_URI&response_type=code&scope=https://www.googleapis.com/auth/userinfo.email+https://www.googleapis.com/auth/userinfo.profile" class="text-blue-600 hover:underline">Sign Up with Google</a>
+                </div>
+            </form>
+        </div>
+    </div>
+    <!--include footer-->
+    <?php include("./layouts/footer.php"); ?>
+</body>
+
+</html>
