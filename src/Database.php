9328886cbd2a8a6c347b0bb84d25ba2ba95bd8d3
Gulam Kibria Chowdhury <43819400+GK-CPP@users.noreply.github.com>
2023-09-20 17:22:32 +0600

    conn database PDO

--- src/Database.php
+++ src/Database.php
@@ -1,10 +1,41 @@
 <?php
 
+//database conection
+
+class Database
+{
+
+  private $servername = 'localhost';
+  private $username = 'root';
+  private $password = '';
+  private $dbname = 'oop_crud';
+  private $conn;
+
+  function __construct()
+  {
+    try {
+      //pdo database
+      $this->conn = new PDO("mysql:host=$this->servername;dbname=$this->dbname", $this->username, $this->password);
+      $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+      die("Connection failed: " . $e->getMessage());
+    }
+  }
 
 
-class Database {
-
-  static function getInstace() {
-    return null;
+  public function connect()
+  {
+    return $this->conn;
+  }
+  public function prepare($sql)
+  {
+    return $this->conn->prepare($sql);
+  }
+  public function close()
+  {
+    $this->conn = null;
   }
 }
+
+//check database connection
+//$db = new Database();
