ba2c59bd673268191bb3748aa0a19f83745e9775
Gulam Kibria Chowdhury <43819400+GK-CPP@users.noreply.github.com>
2023-09-20 17:23:06 +0600

    store function write

--- src/models/Product.php
+++ src/models/Product.php
@@ -1,45 +1,80 @@
 <?php
 
-require_once '../Database.php';
+require_once '../utils.php';
+require_once '../src/database.php';
 
-use Database;
+//use Database;
 
-class Product {
+class Product
+{
   private $name;
-  private $price;
+  private $email;
+  private  $password;
+  private $role;
+  private $description;
 
   // constructor with name and price
-  public function __construct($name, $price) {
+  public function __construct($name, $email, $password, $role, $description)
+  {
+    $this->name = $name;
+    $this->email = $email;
+    $this->password = $password;
+    $this->role = $role;
+    $this->description = $description;
+    //dd($this);
+  }
+  public function store()
+  {
+    //print or show all the data for test
 
+    try {
+      //dd(($this));
+      print_r("query executed");
+      $db = new Database();
+      $db->connect();
+      $sql = "INSERT INTO student (name, email, password, role, description) VALUES (:name, :email, :password, :role, :description)";
+      $stmt = $db->prepare($sql);
+      $stmt->bindParam(':name', $this->name);
+      $stmt->bindParam(':email', $this->email);
+      $stmt->bindParam(':password', $this->password);
+      $stmt->bindParam(':role', $this->role);
+      $stmt->bindParam(':description', $this->description);
+      print_r("query executed 2");
+      $stmt->execute();
+      print_r("query executed 3");
+
+      return true;
+    } catch (\PDOException $e) {
+      echo $e->getMessage();
+      return false;
+    }
   }
 
-  public function store() {
-    $db = Database::getInstance();
 
-    // fire a query to store the product
+  //   public function index()
+  //   {
+  //   }
 
-    $db->query("INSERT INTO products (name, price) VALUES (:name, :price)");
+  // public function show($id)
+  // {
+  //   //create a database class object
 
-    $db->bind(':name', $this->name);
-  }
 
-  public function index() {
 
-  }
+  // }
 
-  public function show($id) {
+  //   public function update($name, $price)
+  //   {
+  //   }
 
-  }
+  //   public function delete($id)
+  //   {
+  //   }
 
-  public function update($name, $price) {
-
-  }
-
-  public function delete($id) {
-  }
-
-  // destructor
-  public function __destruct() {
-    $db->close();
-  }
+  //   // destructor
+  //   public function __destruct()
+  //   {
+  //     $db->close();
+  //   }
+  // }
 }
