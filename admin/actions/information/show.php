eaca47dd06cc398cdf4001eea59cb2ef7ba752d6
Gulam Kibria Chowdhury <43819400+GK-CPP@users.noreply.github.com>
2023-09-25 20:01:32 +0600

    show page change

--- admin/actions/information/show.php
+++ admin/actions/information/show.php
@@ -1,12 +1,29 @@
-<!-- <?php
+<?php
+
+use App\Models\StudentInfo;
+
+// show_all funtion data recived from database
+$obj = new StudentInfo('', '', '', ',', '', '', '');
+$users = $obj->show_all();
 
 
-        //require '../src/Product.php';
-
-        //use Product;
-
-        //$productObj = new Product();
-
-        //$products = $productObj->index();
-
-        ?> -->
+// foreach ($users as $user) {
+//         echo $user['name'];
+//         echo '<br>';
+//         echo $user['userid'];
+//         echo '<br>';
+//         echo $user['level'];
+//         echo '<br>';
+//         echo $user['group'];
+//         echo '<br>';
+//         echo $user['roll'];
+//         echo '<br>';
+//         echo $user['role'];
+//         echo '<br>';
+//         echo $user['description'];
+//         echo '<br>';
+//         echo '<br>';
+// }
+// echo '<pre>';
+// var_dump(count($data));
+// print_r($data[0]['name']);
